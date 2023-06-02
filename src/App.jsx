d17900d3bcbd68cd53aa07590da6e7b99d9ffab4
Mauricio Soria <mauricioutn2017@gmail.com>
2023-06-02 00:02:44 -0300

    correccion de etiquetas meta y nombre

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Titulo from "./components/Titulo"
 import Boton from './components/Boton';
 import { useState } from "react";
-
+import "./App.css";
+import Footer from './components/Footer'
 function App() {
   const [estado, setEstado] = useState('');
 
@@ -10,6 +11,7 @@ function App() {
     <>
     <Titulo nuevoTitulo='My friend!' estado={estado}></Titulo>
     <Boton setEstado={setEstado}></Boton>
+    <Footer></Footer>
     </>
   )
 }
--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -2,7 +2,7 @@
 const Boton = ({setEstado}) => {
     return (
         <div>
-        <button className="btn btn-primary" onClick={()=>setEstado('(from changed state)!')}>Click me to add!</button>
+        <button className="btn btn-primary" onClick={()=>setEstado(',(from changed state)!')}>Click me to add!</button>
         <button className="btn btn-danger" onClick={()=>setEstado('')}>Click me to delete!</button>
         </div>
     );
